c127ad023ef3efd982955dc0d66a2e5d28a7e1ce
noisak <ilidar228@gmail.com>
2018-11-15 17:44:40 +0300

    complete 3 task

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,15 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+   if (year % 4 != 0)
+      return false;
+   else if (year % 100 != 0)
+      return true;
+   else if (year % 400 != 0)
+      return false;
+   else
+       return true;
 }
 
 
@@ -76,7 +84,18 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let dif = endDate - startDate;
+   let ms = parseInt(dif % 1000);
+   let sec = parseInt(dif/1000 % 60);
+   let min = parseInt(dif/60000 % 60);
+   let hour = parseInt(dif/3600000 % 24);
+   if(hour < 10) hour = '0' + hour;      
+   if(min < 10) min = '0' + min;
+   if(sec < 10) sec = '0' + sec;
+   if(ms < 10) ms = '00' + ms;
+   else if(ms<100) ms = '0' + ms;
+   return hour + ":" + min + ":" + sec + "." + ms;
+
 }
 
 
@@ -94,8 +113,9 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
-}
+    let angle = Math.abs((Math.PI/360) * (60*(date.getUTCHours() % 12) - 11*(date.getMinutes())));
+    return Math.min(angle, 2*Math.PI - angle);
+   }
 
 
 module.exports = {
